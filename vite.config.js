ad236ca17df99e46343f1acd4fd31c5e3e8acb51
santhosh-v11 <santhosh592002@gmail.com>
2026-01-23 17:12:41 +0530

    fix:path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        Task1: resolve(__dirname, "Pages/Task1/index.html"),
+        Task2: resolve(__dirname, "Pages/Task2/index.html"),
+        Task3: resolve(__dirname, "Pages/Task3/index.html"),
+        Task4: resolve(__dirname, "Pages/Task4/index.html"),
+      },
+    },
+  },
 })
